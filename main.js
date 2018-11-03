5034de78077edd842a81e6da396bbb414f427bac
huy-ha <haquochuy@gmail.com>
2018-11-03 02:59:30 -0400

    added stats

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 //Plane variables
-var size = 20;
-var detail = 10;
+var size = 20; // 20 works
+var detail = 2;
 var height = 2;
+var bumps = 3;
 
 var scene,camera,renderer;
 
@@ -12,34 +13,34 @@ var time = 0;
 init();
 
 function init(){
+	detail *= size;
 	scene = new THREE.Scene();
-
 	camera = new THREE.PerspectiveCamera(30,window.innerWidth/window.innerHeight,0.1,1000);
-	//camera.lookAt({
-	//	x: 0,
-        //	y: 0,
-        //	z: 0
-    	//});
 
 	camera.position.z = 50;
 	camera.position.y = 50;
 	camera.rotation.x = -Math.PI/4;
 
-	
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight);
 	renderer.shadowMap.enabled = true;
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 	document.body.appendChild( renderer.domElement );
 	
+	createPlane();
+	addLights();
+}
+
+function createPlane(){
 	//creating the plane
 	var geometry = new THREE.PlaneBufferGeometry(size,size,detail,detail);
 	var position = geometry.attributes.position;
 	position.dynamic = true;
 
+	console.log("plane with " + position.count + " vertices");
 	//changing plane height
 	for(var i = 0; i < position.count; i++)
-		position.setZ(i, height*Math.sin(i/2)*Math.cos(i+4));
+		position.setZ(i, height * Math.sin( i/detail ) * Math.cos(i/detail));
 	
 	var material = new THREE.MeshLambertMaterial( {color: 0xa5b1c9});
 	plane = new THREE.Mesh(geometry, material);
@@ -48,10 +49,9 @@ function init(){
 	plane.rotation.x = -Math.PI/2;
 	plane.rotation.z = Math.PI/4;
 	scene.add(plane);
+}
 
-	//test shadows
-	createCube();
-	
+function addLights(){
 	//main lights
 	directLight = new THREE.PointLight( 0xFFFFFF, 0.7, 0, 2);
 	directLight.position.z = 5;
@@ -64,24 +64,7 @@ function init(){
 
 	scene.add( directLight);
 	scene.add( new THREE.AmbientLight( 0x404040 ));
-
 }
-
-function createCube(){
-	var geometry = new THREE.CubeGeometry(3,3,3);
-	var material = new THREE.MeshLambertMaterial( {color: 0x01a6ff});
-	var cube = new THREE.Mesh(geometry, material);
-	cube.position.y = 5;
-	cube.rotation.x = 0.3;
-	cube.rotation.z = 0.7;
-	cube.castShadow = true;
-
-	scene.add(cube);
-}
-
-
-
-
 
 function animate(){
 	time += 0.01;
@@ -96,6 +79,6 @@ function animate(){
 animate();
 
 function updateRotation(){
-	directLight.position.set( 10*Math.sin(time) , 5 , 10*Math.cos(time) );
+	directLight.position.set( 10*Math.sin(time) , 5 , 10*Math.cos(time));
 }
 
